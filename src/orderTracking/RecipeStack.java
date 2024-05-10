d80ef1afe1e0abf07bb5e19cb0d135c4acfcf395
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-05-10 23:50:11 +0200

    Class - RecipeStack - created constructor, getters and setters, toString() Captured exceptions in the constructor (for variable with BigDecimal, String values, Duration) Also added a method to check if URL is valid.

---
+++ src/orderTracking/RecipeStack.java
@@ -0,0 +1,93 @@
+package orderTracking;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RecipeStack {
+    private String title;
+    private BigDecimal price;
+    private  Duration preparationTime;
+    private  String imageUrl;
+
+    public RecipeStack(String title, BigDecimal price, Duration preparationTime, String imageUrl) {
+        if(title == null) {
+            throw new NullPointerException("The title is not allowed to be a null value");
+        }
+        this.title = title;
+
+        if(price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw  new IllegalArgumentException("The value must be positive!");
+        }
+        this.price = price;
+
+        if(preparationTime.isNegative() || preparationTime.isZero()) {
+            throw new IllegalArgumentException("The duration of a recipe cannot be zero or a negative number. Enter a positive value.");
+        }
+        this.preparationTime = preparationTime;
+
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title == null) {
+            throw new NullPointerException("The title is not allowed to be a null value");
+        }
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        if(price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw  new IllegalArgumentException("The value must be positive!");
+        }
+        this.price = price;
+    }
+
+    public Duration getPreparationTime() {
+        if(preparationTime.isNegative() || preparationTime.isZero()) {
+            throw new IllegalArgumentException("The duration of a recipe cannot be zero or a negative number. Enter a positive value.");
+        }
+        return preparationTime;
+    }
+
+    public void setPreparationTime(Duration preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
+    public static boolean checkURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            return true; //URL is valid
+        } catch (MalformedURLException e) {
+            return false; //URL is not valid
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeStack{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", preparationTime=" + preparationTime +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
