b33b832bffa90befcc0a38911cb8b02d28699320
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-06-16 23:00:34 +0200

    A new package has been created - FileHandling. It separately defines how to save the CookBook and Order to a file and reload from the file of CookBook and Order for better visibility.

---
+++ src/FileHandling/fileHandlingCookBook.java
@@ -0,0 +1,10 @@
+package FileHandling;
+
+public class fileHandlingCookBook {
+
+    /* 1. Save COOKBOOK to file and Load the COOKBOOK from file:
+       2. Save ORDER to file and load the ORDER from file:
+     */
+
+
+}
