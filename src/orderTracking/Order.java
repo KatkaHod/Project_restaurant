e88507cf3d26e9873f123938953cd6d844923389
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-06-16 20:19:56 +0200

    Order - the class is done

--- src/orderTracking/Order.java
+++ src/orderTracking/Order.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
-    private Dish mealName;
+    private Dish dishName;
     private Table table;
     private int quantity;
     private LocalDateTime orderedTime;
-    private LocalDateTime fulfilmentTime; //the time - order is completed
+    private LocalDateTime fulfilmentTime;
     private boolean paid;
     private List<Order> listOfOrders = new java.util.ArrayList<>();
 
 
-    public Order(Dish dishName, int quantity, Table table,LocalDateTime fulfilmentTime) {
-        this.mealName = dishName;
+    public Order(Dish dishName, int quantity, Table table) {
+        this.dishName = dishName;
         this.quantity = quantity;
         this.table = table;
         this.orderedTime = LocalDateTime.now();
@@ -24,10 +24,12 @@ public class Order {
         this.paid = false; // Order not yet paid
     }
 
+    //mark when the order is done
     public void markAsFulfilled() {
         this.fulfilmentTime = LocalDateTime.now();
     }
 
+    //mark as paid
     public void markAsPaid() {
         this.paid = true;
     }
@@ -35,7 +37,7 @@ public class Order {
     @Override
     public String toString() {
         return "Order{" +
-                "mealName=" + mealName +
+                "mealName=" + dishName +
                 ", table=" + table +
                 ", quantity=" + quantity +
                 ", orderedTime=" + orderedTime +
