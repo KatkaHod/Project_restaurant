eaea4ec27f4473bd59adade01a32d3a2d31d5d8d
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-06-30 22:43:23 +0200

    FileOperations - in progress

--- src/Order/Orders.java
+++ src/Order/Orders.java
@@ -7,66 +7,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.PrintWriter;
 
 public class Orders {
-    private List<Order> listOrders = new java.util.ArrayList<>();
+    private List<Order> orders = new java.util.ArrayList<>();
 
-    public List<Order> getListOrders() {
-        return new ArrayList<>(listOrders);
+    public List<Order> getOrders() {
+        return new ArrayList<>(orders);
     }
 
     public void addOrder(Order newOrder) {
-        listOrders.add(newOrder);
+        orders.add(newOrder);
     }
 
     public Order getOrderViaIndex(int index) {
-        if (index >= 0 && index < listOrders.size()) {
-            return listOrders.get(index);
+        if (index >= 0 && index < orders.size()) {
+            return orders.get(index);
         } else {
             throw new IllegalArgumentException("Invalid index. Provided index: " + index);
         }
     }
 
-    public void loadOrdersFromFile(String inputFilename, String delimiter) throws OrderException {
-        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(inputFilename)))) {
-            String line;
-            int lineNumber = 0;
-            while (scanner.hasNextLine()) {
-                lineNumber++;
-                line = scanner.nextLine();
-                String[] parts = line.split(delimiter);
-                //orderTracking.Order order = parseOrder(parts, lineNumber, line);
-                //addOrder(order);
-            }
-        } catch (IOException e) {
-            throw new OrderException("Error during file read process: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            throw new OrderException("Unknown category: " + e.getMessage());
+    public void printOrders() {
+        for (Order order : orders) {
+            System.out.println(order.toString());
         }
-    }
-
-    public void saveOrdersToFile(String filename, String delimiter) throws OrderException {
-        try (PrintWriter writer = new PrintWriter(filename)) {
-            for (Order order : listOrders) {
-                writer.println(
-                        order.getDishID() + delimiter
-                                + order.getQuantity() + delimiter
-                                + order.getTableNumber() + delimiter
-                                + order.getOrderedTime() + delimiter
-                                + order.getFulfilmentTime() + delimiter
-                                + (order.isPaid() ? "yes" : "no"));
-            }
-        } catch (IOException e) {
-            throw new OrderException("Error when writing to file `" +filename+ "`: " + e.getMessage());
-        }
-    }
-
-
-    @Override
-    public String toString() {
-        return "Orders{" +
-                "listOrders=" + listOrders +
-                '}';
     }
 }
--- src/Settings/FileOperations.java
+++ src/Settings/FileOperations.java
@@ -1,14 +1,64 @@
 package Settings;
 
+import Order.OrderException;
+
 import java.io.*;
 import java.time.*;
 import java.util.Map;
 import java.math.BigDecimal;
 import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.Scanner;
 
+import Order.Order;
+import Order.Orders;
+import Order.OrderException;
 import static Settings.Settings.getDelimiter;
 
 public class FileOperations {
+
+
+    //Orders
+
+    public void saveOrdersToFile(String filename, String delimiter) throws OrderException {
+        try (PrintWriter writer = new PrintWriter(filename)) {
+            for (Order order : orders) {
+                writer.println(
+                        order.getDishID() + delimiter
+                                + order.getQuantity() + delimiter
+                                + order.getTableNumber() + delimiter
+                                + order.getOrderedTime() + delimiter
+                                + order.getFulfilmentTime() + delimiter
+                                + (order.isPaid() ? "yes" : "no"));
+            }
+        } catch (IOException e) {
+            throw new OrderException("Error when writing to file `" +filename+ "`: " + e.getMessage());
+        }
+    }
+
+
+    public void loadOrdersFromFile(String inputFilename, String delimiter) throws OrderException {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(inputFilename)))) {
+            String line;
+            int lineNumber = 0;
+            while (scanner.hasNextLine()) {
+                lineNumber++;
+                line = scanner.nextLine();
+                String[] parts = line.split(delimiter);
+                //orderTracking.Order order = parseOrder(parts, lineNumber, line);
+                //addOrder(order);
+            }
+        } catch (IOException e) {
+            throw new OrderException("Error during file read process: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw new OrderException("Unknown category: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
 
 }
