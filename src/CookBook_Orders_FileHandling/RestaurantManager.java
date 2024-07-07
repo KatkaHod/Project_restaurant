bffc7f39d5e9ca3586054d82e5040395915565b0
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-07 18:44:13 +0200

    Added the method to sort order by orderedTime (class Order) - sortOrdersByOrdertime() - Using lamba expression

--- src/CookBook_Orders_FileHandling/RestaurantManager.java
+++ src/CookBook_Orders_FileHandling/RestaurantManager.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 public class RestaurantManager {
 
-    //Count of orders that are currently in progress -
-
+    //1a. Count of orders that are currently in progress -
     public int countPendingOrders(List<Order> orders) {
         return (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() == null && order.getOrderedTime() != null)
                 .count();
     }
 
-    //Count of orders that are not finished - the order is not paid yet
+    //1b. Count of orders that are not finished - the order is not paid yet
     public int countUnfinishedOrders(List<Order> orders) {
         return (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() != null && !order.isPaid())
                 .count();
     }
+
+    //2. Sort orders by order time
+    public void sortOrdersByOrderTime(List<Order> orders) {
+        orders.sort((order1, order2) -> order1.getOrderedTime().compareTo(order2.getOrderedTime()));
+    }
+
+
+
+
 
 
 
