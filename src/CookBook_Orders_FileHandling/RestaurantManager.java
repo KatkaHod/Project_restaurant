77e03b661f986ac8f3889008bd81918111f22889
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-07 19:59:40 +0200

    added method - calculateAverageFulfilmentTime - done

--- src/CookBook_Orders_FileHandling/RestaurantManager.java
+++ src/CookBook_Orders_FileHandling/RestaurantManager.java
@@ -1,36 +1,37 @@
 package CookBook_Orders_FileHandling;
 
+import java.time.Duration;
 import java.util.List;
 
 public class RestaurantManager {
 
-    //1a. Count of orders that are currently in progress -
+    //1. Count of orders that are currently in progress
     public int countPendingOrders(List<Order> orders) {
         return (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() == null && order.getOrderedTime() != null)
                 .count();
     }
 
-    //1b. Count of orders that are not finished - the order is not paid yet
+    //2. Count of orders that are not finished - the order is not paid yet
     public int countUnfinishedOrders(List<Order> orders) {
         return (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() != null && !order.isPaid())
                 .count();
     }
 
-    //2. Sort orders by order time
+    //3. Orders sorted by time
     public void sortOrdersByOrderTime(List<Order> orders) {
         orders.sort((order1, order2) -> order1.getOrderedTime().compareTo(order2.getOrderedTime()));
     }
 
-
-
-
-
-
-
-
-
+    //4. average fulfilment time in minutes
+    public double calculateAverageFulfilmentTime(List<Order> orders) {
+        return orders.stream()
+                .filter(order -> order.getFulfilmentTime() != null && !order.getOrderedTime().isAfter(order.getFulfilmentTime()))
+                .mapToLong(order -> Duration.between(order.getOrderedTime(), order.getFulfilmentTime()).toMinutes())
+                .average()
+                .orElseThrow(() -> new IllegalArgumentException("No completed orders to calculate average fulfilment time."));
+    }
 
 
 }
