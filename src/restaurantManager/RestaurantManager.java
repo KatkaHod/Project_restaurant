d96ac53497e1a8ed0ef0e3a19447b3bed7e52895
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-07 21:20:31 +0200

    All methods are corrected -> every methods print the result (calculation)

--- src/restaurantManager/RestaurantManager.java
+++ src/restaurantManager/RestaurantManager.java
@@ -9,16 +9,20 @@ public class RestaurantManager {
 
     //1. Count of orders that are currently in progress
     public int countPendingOrders(List<Order> orders) {
-        return (int) orders.stream()
+        int pendingOrdersCount = (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() == null && order.getOrderedTime() != null)
                 .count();
+        System.out.println("Count of orders in progress: " + pendingOrdersCount);
+        return pendingOrdersCount;
     }
 
     //2. Count of orders that are not finished - the order is not paid yet
     public int countUnfinishedOrders(List<Order> orders) {
-        return (int) orders.stream()
+        int unfinishedOrders = (int) orders.stream()
                 .filter(order -> order.getFulfilmentTime() != null && !order.isPaid())
                 .count();
+        System.out.println("Count of unfinished orders: " + unfinishedOrders);
+        return unfinishedOrders;
     }
 
     //3. Orders sorted by time
@@ -28,12 +32,18 @@ public class RestaurantManager {
 
     //4. average fulfilment time in minutes
     public double calculateAverageFulfilmentTime(List<Order> orders) {
-        return orders.stream()
+        double averageMinutes = orders.stream()
                 .filter(order -> order.getFulfilmentTime() != null && !order.getOrderedTime().isAfter(order.getFulfilmentTime()))
                 .mapToLong(order -> Duration.between(order.getOrderedTime(), order.getFulfilmentTime()).toMinutes())
                 .average()
                 .orElseThrow(() -> new IllegalArgumentException("No completed orders to calculate average fulfilment time."));
+        System.out.printf("Average fulfilment time: %.2f minutes%n", averageMinutes);
+
+        return averageMinutes;
     }
+
+    //5.
+
 
 
 }
