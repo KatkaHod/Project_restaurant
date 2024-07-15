d9dcf087c53345a6c33f331e20879c951a96fe06
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-15 19:10:34 +0200

    Added dishes to CookBook

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,51 @@
-import Tracking.CookBook;
-import Tracking.FileOperations;
-import Tracking.Order;
-import Tracking.Settings;
+import Tracking.*;
+import restaurantExceptions.CookBookException;
+import restaurantExceptions.DishException;
+import restaurantExceptions.FileException;
+import restaurantExceptions.OrderException;
+import restaurantManager.RestaurantManager;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Instance of class Order and cookBook
-        List<Order> orders = new ArrayList<>();
+        //Instance of classes
+        Orders orders = new Orders();
         CookBook cookBook = new CookBook();
+        RestaurantManager restaurantManager = new RestaurantManager();
 
-        //1. Load from the file - at this moment it is empty
 
+        //1. Load from the file - It is empty at the moment
         String fileOrders = Settings.getFileNameOrders();
         String fileCookBook = Settings.getFileNameCookBook();
+        FileOperations fileOperations = new FileOperations();
 
-        //???????????
-        FileOperations.loadOrdersFromFile(orders,cookBook,fileOrders);
+        try {
+            fileOperations.loadCookBookFromFile(cookBook,fileCookBook);
+            fileOperations.loadOrdersFromFile(orders.getOrders(),cookBook,fileOrders);
+        } catch (FileException | OrderException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
+        //2. Add dishes to CookBook
 
-
-
-
-
-
-
-
-
-
+        try {
+            cookBook.addDish(new Dish("Chicken fillet 150 g", BigDecimal.valueOf(250), 45, "Chicken-fillet "));
+            cookBook.addDish(new Dish("Fries 150 g", BigDecimal.valueOf(120), 20, "Fries"));
+            cookBook.addDish(new Dish("Trout on wine 200 g", BigDecimal.valueOf(400), 45,"Trout-on-wine "));
+            cookBook.addDish(new Dish("Kofola drink 0,5 l", BigDecimal.valueOf(50), 5, "Kofola"));
+        } catch (CookBookException | DishException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
 
 
 
     }
+
+
+
+
+
 }
