c7a480a45b29fbd8e75b48aa1c4a0e2b7ad67c5f
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-07-15 20:02:16 +0200

    Main - orders created. -> #The customers (two people) at table 15 have ordered 2x chickens, 2x fries and two drinks. They have now received their drink. Still waiting for the 2x chicken.
    #The customers (four people) at table 2 have ordered 3 x chicken, 4 x fries, 1 x fish and 4 x drinks. They have now received their drink.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ import restaurantExceptions.OrderException;
 import restaurantManager.RestaurantManager;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Instance of classes
         Orders orders = new Orders();
         CookBook cookBook = new CookBook();
         RestaurantManager restaurantManager = new RestaurantManager();
@@ -28,8 +28,8 @@ public class Main {
             System.out.println("Error: " + e.getMessage());
         }
 
-        //2. Add dishes to CookBook
 
+        //2. Add dishes to CookBook
         try {
             cookBook.addDish(new Dish("Chicken fillet 150 g", BigDecimal.valueOf(250), 45, "Chicken-fillet "));
             cookBook.addDish(new Dish("Fries 150 g", BigDecimal.valueOf(120), 20, "Fries"));
@@ -38,6 +38,28 @@ public class Main {
         } catch (CookBookException | DishException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        //3. Create new orders for table 15 and table 2 - Drinks for both orders are be served immediately
+        try {
+            orders.addOrder(new Order(cookBook.getDish(1),2,15));
+            orders.addOrder(new Order(cookBook.getDish(2),2,15));
+            orders.addOrder(new Order(cookBook.getDish(4),2, LocalDateTime.now(), LocalDateTime.now(),15, Boolean.FALSE));
+
+            orders.addOrder(new Order(cookBook.getDish(1),3,2));
+            orders.addOrder(new Order(cookBook.getDish(2),4,2));
+            orders.addOrder(new Order(cookBook.getDish(3),1,2));
+            orders.addOrder(new Order(cookBook.getDish(4),4, LocalDateTime.now(), LocalDateTime.now(),2, Boolean.FALSE));
+        } catch (OrderException | CookBookException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+
+
+
+
+
 
 
 
